b149efea5ce8b61b422601a814e9c54bcc8d669a
bookswap <bookswap2021@gmail.com>
2021-02-27 14:29:44 +0800

    init

---
+++ precache-manifest.0e17d9c526e634e95d28ab41dac5cca5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96a79eba8973fda4263",
+    "url": "/static/css/main.c44d58a1.chunk.css"
+  },
+  {
+    "revision": "f96a79eba8973fda4263",
+    "url": "/static/js/main.f96a79eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "bd39bbea00df98922c90",
+    "url": "/static/css/2.d4efab6b.chunk.css"
+  },
+  {
+    "revision": "bd39bbea00df98922c90",
+    "url": "/static/js/2.bd39bbea.chunk.js"
+  },
+  {
+    "revision": "e63b47bd5bb7736050633edb20f8ef99",
+    "url": "/static/media/bank.e63b47bd.png"
+  },
+  {
+    "revision": "3689fcff0dd763b966398c156e5ea170",
+    "url": "/static/media/about.3689fcff.png"
+  },
+  {
+    "revision": "9697fadce5cf23f8af252cb5c5c13cd3",
+    "url": "/static/media/logo.9697fadc.png"
+  },
+  {
+    "revision": "b984d454bc2e3cb96bc601ae9ec8107c",
+    "url": "/static/media/distributionCharten.b984d454.png"
+  },
+  {
+    "revision": "935133f12e87792f452ce200e47fe200",
+    "url": "/static/media/distributionChartzh_CN.935133f1.png"
+  },
+  {
+    "revision": "e5df5464842ed9bd7d4ed6787432e79c",
+    "url": "/static/media/sloganen.e5df5464.png"
+  },
+  {
+    "revision": "ab6825434a95a46f4fcae66e802cd30e",
+    "url": "/static/media/sloganzh_CN.ab682543.png"
+  },
+  {
+    "revision": "2943dabc71fa92a115274cca94a96c9d",
+    "url": "/static/media/bankSelected.2943dabc.png"
+  },
+  {
+    "revision": "f7144cebd9615fae28b2959ef1c672d7",
+    "url": "/static/media/aboutSelected.f7144ceb.png"
+  },
+  {
+    "revision": "00b10f8ea40aeab6dd3b2039c8f264d6",
+    "url": "/static/media/safe.00b10f8e.png"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "/static/media/mdx.8e72f6f2.png"
+  },
+  {
+    "revision": "0054a165bf47a4dcd103af1fd323791b",
+    "url": "/static/media/usdt.0054a165.png"
+  },
+  {
+    "revision": "d95f29fc70c27677a11f28c5c0429164",
+    "url": "/static/media/audit.d95f29fc.pdf"
+  },
+  {
+    "revision": "c48422f15f8e1624999dc313387dca9a",
+    "url": "/static/media/bg.c48422f1.png"
+  },
+  {
+    "revision": "0eb504c549d853605958290e4a42a3fc",
+    "url": "/static/media/medium.0eb504c5.png"
+  },
+  {
+    "revision": "e216d61efa48ae6437431f9154e4b0db",
+    "url": "/static/media/twitter.e216d61e.png"
+  },
+  {
+    "revision": "8259c6c6f9ce903ec90cecf83fb7ad16",
+    "url": "/index.html"
+  }
+];
